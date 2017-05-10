e2e74801ef0886811f0285d33de451c1b23ab410
linweiyun <linweiyun22@outlook.com>
2017-05-10 11:06:41 +0800

    20170510

--- public/js/pages.js
+++ public/js/pages.js
@@ -11,26 +11,28 @@ $('#homePage').on("click", function () {
             $('#resultList').append('<tbody></tbody>');
             for (var i = 0; i < msg.length; i++) {
                 var str = "<tr><td><a class=\'text\' href=\"/result/?view=" + msg[i].ID + "\" target=\'_blank\'>" + msg[i].title + "</a></td>";
-                str += "<td class=\'center aligned\'>"+msg[i].username+"</td>";
-                str += "<td class=\'center aligned\'>"+msg[i].category+"</td>";
-                str += "<td class=\'center aligned\'>"+msg[i].size+"</td></tr>";
+                str += "<td class=\'center aligned\'>" + msg[i].username + "</td>";
+                str += "<td class=\'center aligned\'>" + msg[i].category + "</td>";
+                str += "<td class=\'center aligned\'>" + msg[i].size + "</td></tr>";
                 $('#resultList tbody').append(str);
             }
             $('#pageNumber').text('1');
             $('#prePage').attr('data-value', '0');
             $('#nextPage').attr('data-value', '2');
-            $('#prePage').css("display",'none');
-            $('#homePage').css("display",'none');
+            $('#prePage').css("display", 'none');
+            $('#homePage').css("display", 'none');
         });
 });
 
 $('#prePage').on("click", function () {
     var searchValue = $('#search-value').text();
     var pageNum = $('#pageNumber').text();
+    var filterValue = $('[data-click = 1]').attr('data-value');
+    // console.log(filterValue);
     $.ajax({
         method: "GET",
         url: "/pages",
-        data: {search: searchValue, idx: (pageNum*1-1)}
+        data: {search: searchValue, idx: (pageNum * 1 - 1),filter:filterValue}
     })
         .done(function (msg) {
             console.log(msg[0]);
@@ -38,51 +40,53 @@ $('#prePage').on("click", function () {
             $('#resultList').append('<tbody></tbody>');
             for (var i = 0; i < msg.length; i++) {
                 var str = "<tr><td><a class=\'text\' href=\"/result/?view=" + msg[i].ID + "\" target=\'_blank\'>" + msg[i].title + "</a></td>";
-                str += "<td class=\'center aligned\'>"+msg[i].username+"</td>";
-                str += "<td class=\'center aligned\'>"+msg[i].category+"</td>";
-                str += "<td class=\'center aligned\'>"+msg[i].size+"</td></tr>";
+                str += "<td class=\'center aligned\'>" + msg[i].username + "</td>";
+                str += "<td class=\'center aligned\'>" + msg[i].category + "</td>";
+                str += "<td class=\'center aligned\'>" + msg[i].size + "</td></tr>";
                 $('#resultList tbody').append(str);
             }
             if ((pageNum * 1 - 1) == 1) {
                 $('#prePage').attr('data-value', '0');
                 $('#nextPage').attr('data-value', '2');
-                $('#prePage').css("display",'none');
-                $('#homePage').css("display",'none');
+                $('#prePage').css("display", 'none');
+                $('#homePage').css("display", 'none');
                 $('#pageNumber').text('1');
             } else {
                 $('#prePage').attr('data-value', '' + (pageNum * 1 - 2) + '');
                 $('#nextPage').attr('data-value', '' + pageNum * 1 + '');
-                $('#pageNumber').text(''+(pageNum * 1-1)+'');
+                $('#pageNumber').text('' + (pageNum * 1 - 1) + '');
             }
         });
 });
 
 $('#nextPage').on("click", function () {
     var searchValue = $('#search-value').text();
-    var idx = $('#nextPage').attr('data-value');
+    // var idx = $('#nextPage').attr('data-value');
+    var filterValue = $('[data-click = 1]').attr('data-value');
+    // console.log(filterValue);
     var pageNum = $('#pageNumber').text();
     var totalResult = $('#totalResult').text();
     if (Math.ceil(totalResult * 1 / 15) > pageNum) {
         $.ajax({
             method: "GET",
             url: "/pages",
-            data: {search: searchValue, idx: pageNum*1+1}
+            data: {search: searchValue, idx: pageNum * 1 + 1,filter:filterValue}
         })
             .done(function (msg) {
                 $('#resultList tbody').remove();
                 $('#resultList').append('<tbody></tbody>');
                 for (var i = 0; i < msg.length; i++) {
                     var str = "<tr><td><a class=\'text\' href=\"/result/?view=" + msg[i].ID + "\" target=\'_blank\'>" + msg[i].title + "</a></td>";
-                    str += "<td class=\'center aligned\'>"+msg[i].username+"</td>";
-                    str += "<td class=\'center aligned\'>"+msg[i].category+"</td>";
-                    str += "<td class=\'center aligned\'>"+msg[i].size+"</td></tr>";
+                    str += "<td class=\'center aligned\'>" + msg[i].username + "</td>";
+                    str += "<td class=\'center aligned\'>" + msg[i].category + "</td>";
+                    str += "<td class=\'center aligned\'>" + msg[i].size + "</td></tr>";
                     $('#resultList tbody').append(str);
                 }
                 $('#prePage').attr('data-value', '' + pageNum * 1 + '');
                 $('#nextPage').attr('data-value', '' + (pageNum * 1 + 2) + '');
-                $('#pageNumber').text(''+(pageNum * 1+1)+'');
-                $('#prePage').css("display",'');
-                $('#homePage').css("display",'');
+                $('#pageNumber').text('' + (pageNum * 1 + 1) + '');
+                $('#prePage').css("display", '');
+                $('#homePage').css("display", '');
             });
     }
     else {
@@ -104,10 +108,45 @@ $('#searchBtn').on("click", function () {
     }
 });
 
-$('.filterSpan').on('mouseenter',function () {
-   $(this).css("background-color","yellow");
+$('.filterSpan').on('mouseenter', function () {
+    $(this).css({"background-color": "#CCCCCC", "color": "white"});
 });
 
-$('.filterSpan').on('mouseleave',function () {
-    $(this).css("background-color","");
+$('.filterSpan').on('mouseleave', function () {
+    if($(this).attr('data-click') == 0){
+        console.log('in');
+        $(this).css({"background-color": "", "color": "black"});
+    }
+});
+
+$('.filterSpan').on('click', function () {
+    $(this).css({"background-color": "#CCCCCC", "color": "white"});
+    $(this).siblings().css({"background-color": "", "color": "black"});
+    $(this).siblings().attr('data-click','0');
+    $(this).attr('data-click','1');
+    var searchValue = $('#search-value').text();
+    var filterValue = $(this).attr('data-value');
+    $.ajax({
+        method: "GET",
+        url: "/pages",
+        data: {search: searchValue, idx: "1", filter: filterValue,flag:1}
+    })
+        .done(function (msg) {
+            //console.log("Data Saved: " + msg);
+            $('#resultList tbody').remove();
+            $('#resultList').append('<tbody></tbody>');
+            for (var i = 0; i < msg[0].length; i++) {
+                var str = "<tr><td><a class=\'text\' href=\"/result/?view=" + msg[0][i].ID + "\" target=\'_blank\'>" + msg[0][i].title + "</a></td>";
+                str += "<td class=\'center aligned\'>" + msg[0][i].username + "</td>";
+                str += "<td class=\'center aligned\'>" + msg[0][i].category + "</td>";
+                str += "<td class=\'center aligned\'>" + msg[0][i].size + "</td></tr>";
+                $('#resultList tbody').append(str);
+            }
+            $('#pageNumber').text('1');
+            $('#prePage').attr('data-value', '0');
+            $('#nextPage').attr('data-value', '2');
+            $('#prePage').css("display", 'none');
+            $('#homePage').css("display", 'none');
+            $('#totalResult').text(msg[1][0].total);
+        });
 });
--- routers/pages.js
+++ routers/pages.js
@@ -4,13 +4,25 @@
 let express = require('express');
 let router = express.Router();
 let search = require('../db/dbHelper').search;
+let searchJson = require('../db/dbHelper').searchJson;
 let parseAllShare = require('../common/func').parseAllShare;
 
 router.get('/', function (req, res, next) {
     let searchValue = req.query.search;
     let searchIndex = req.query.idx;
-    if(searchValue){
-        search(searchValue,searchIndex)
+    let filterValue = req.query.filter;
+    let first = req.query.flag;
+    // console.log(filterValue);
+    if(first){
+        searchJson(searchValue,filterValue)
+            .then((result) => {
+                result.searchValue = searchValue;
+                result[0] = parseAllShare(result[0],searchValue);
+                res.send(result);
+            });
+    }
+    else if(searchValue){
+        search(searchValue,searchIndex,filterValue)
             .then((result) => {
                 result = parseAllShare(result,searchValue);
                 res.send(result);
