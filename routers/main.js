efd89ce5e8f859de7bdb74a76228698fcecce766
linweiyun <linweiyun22@outlook.com>
2017-07-10 16:17:10 +0800

    20170710 phone adapter finish

--- routers/main.js
+++ routers/main.js
@@ -16,53 +16,53 @@ router.get('/', function (req, res, next) {
     let searchValue = req.query.search;
     let userAgent = req.headers['user-agent'];
     let host = req.headers['host'];
-    console.log(host);
-    if(isPhone(userAgent) &&  host != 'm.91baidupan.com'){
-        res.redirect(`m.91baidupan.com`);
-    }else{
-        // console.log(searchValue);
-        if (searchValue) {
-            sphinxSearch(searchValue)
-                .then(result => {
-                    //console.log(result);
-                    if (result == 'zero') {
-                        let r = [];
-                        r.searchValue = searchValue;
-                        r.totalRecoders = 0;
-                        res.render('main', {results: r});
-                    } else {
-                        let total = result.total;
-                        result = parseAllShare(result, searchValue);
-                        result.searchValue = searchValue;
-                        result.totalRecoders = total;
-                        if (isPhone(userAgent)) {
-                            res.render('mMain', {results: result});
-                        }
-                        else {
-                            res.render('main', {results: result});
-                            // res.render('mMain', {results: result});
-                        }
-
+    // console.log(host);
+    // if(isPhone(userAgent) &&  host != 'm.91baidupan.com'){
+    //     res.redirect(`m.91baidupan.com`);
+    // }else{
+    //
+    // }
+    // console.log(searchValue);
+    if (searchValue) {
+        sphinxSearch(searchValue)
+            .then(result => {
+                //console.log(result);
+                if (result == 'zero') {
+                    let r = [];
+                    r.searchValue = searchValue;
+                    r.totalRecoders = 0;
+                    res.render('main', {results: r});
+                } else {
+                    let total = result.total;
+                    result = parseAllShare(result, searchValue);
+                    result.searchValue = searchValue;
+                    result.totalRecoders = total;
+                    if (isPhone(userAgent)) {
+                        res.render('mMain', {results: result});
+                    }
+                    else {
+                        res.render('main', {results: result});
+                        // res.render('mMain', {results: result});
                     }
 
-                });
-        } else {
-            mainAll().then((result) => {
-                // console.log(result);
-                result[1] = parseAllShare(result[1], '');
-                if (isPhone(userAgent)) {
-                    res.render('mMain', {hots: result});
-                }
-                else {
-                    res.render('main', {hots: result});
-                    // res.render('mMain', {hots: result});
                 }
 
             });
+    } else {
+        mainAll().then((result) => {
+            // console.log(result);
+            result[1] = parseAllShare(result[1], '');
+            if (isPhone(userAgent)) {
+                res.render('mMain', {hots: result});
+            }
+            else {
+                res.render('main', {hots: result});
+                // res.render('mMain', {hots: result});
+            }
 
-        }
+        });
+
     }
-
 
 });
 
