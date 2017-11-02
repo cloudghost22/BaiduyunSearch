d006e123e35a775b40469fb1770b1738a0b3fd6b
linweiyun <linweiyun22@outlook.com>
2017-11-02 11:35:13 +0800

    20171102

--- public/js/ad96090.js
+++ public/js/ad96090.js
@@ -1,2 +1,2 @@
-//960*90 lrs cpc
-document.write('<script src="http://wm.lrswl.com/page/s.php?s=254772&w=960&h=90"></script>');
+//950*90 lrs cpc
+document.write('<script src="http://wm.lrswl.com/page/s.php?s=255035&w=950&h=90"></script>');
